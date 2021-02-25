93851c218e4d114998b50e4d19d10ddd1bbd7674
truth809 <truth809@naver.com>
2021-02-25 21:26:59 +0900

    cartpage12

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { User } = require("../models/User");
 const { Product } = require("../models/Product");
+const { Payment } = require("../models/Payment");
 
 const { auth } = require("../middleware/auth");
 
@@ -178,9 +179,38 @@ router.post('/successBuy', auth, (req, res) => {
     })
 
     // 2. payment collection 안에 자세한 결제 정보들 넣어주기
+    transactionData.user = {
+        id: req.user._id,
+        name: req.user.name,
+        email: req.user.email
+    }
+
+    transactionData.date = req.body.paymentData
+    transactionData.product = history
+
+        // history 정보 저장
+        User.findOneAndUpdate(
+            {_id: req.user._id},
+            { $push: { history: history }, $set: { cart: [] }},
+            { new: true },
+            (err, user) => {
+                if(err) return res.json({ success: false, err })
+
+                // payment에다가 transactionData정보 저장
+                const payment = new Payment(transactionData)
+                payment.save((err, doc) => {
+                    if(err) return res.json({ success: false, err })
 
 
-    // 3. product collection 안에 있는 sold 필드 정보 업데이트 시켜주기
+                    // 3. product collection 안에 있는 sold 필드 정보 업데이트 시켜주기
+                    
+                    
+                })
+                
+            }
+        )
+
+
 
     
     
