9e9462428befcc4f135b9f7772e1fbebc7f13edc
truth809 <truth809@naver.com>
2021-02-25 22:43:46 +0900

    commit

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -15,68 +15,67 @@ function CartPage(props) {
     useEffect(() => {
 
         let cartItems = []
-
-        // 리덕스 user state 안에 cart 안에 상품이 들어있는지 확인
-        if(props.user.userData && props.user.userData.cart) {
-            if(props.user.userData.cart.length > 0) {
+        //리덕스 User state안에 cart 안에 상품이 들어있는지 확인 
+        if (props.user.userData && props.user.userData.cart) {
+            if (props.user.userData.cart.length > 0) {
                 props.user.userData.cart.forEach(item => {
                     cartItems.push(item.id)
                 })
-
                 dispatch(getCartItems(cartItems, props.user.userData.cart))
-                .then(response => { calculateTotal(response.payload)})
-                
+                    .then(response => { calculateTotal(response.payload) })
             }
         }
     }, [props.user.userData])
+
 
     let calculateTotal = (cartDetail) => {
         let total = 0;
 
         cartDetail.map(item => {
-            total += parseInt(item.price,10) * item.quantity
+            total += parseInt(item.price, 10) * item.quantity
         })
+
         setTotal(total)
         setShowTotal(true)
+
     }
+
 
     let removeFromCart = (productId) => {
 
         dispatch(removeCartItem(productId))
-        .then(response => {
+            .then(response => {
 
-            if(response.payload.productInfo.length <= 0) {
-                setShowTotal(false)
-            }
-            
-            
-        })
+                if (response.payload.productInfo.length <= 0) {
+                    setShowTotal(false)
+                }
+
+            })
+
     }
 
     const transactionSuccess = (data) => {
-
         dispatch(onSuccessBuy({
             paymentData: data,
             cartDetail: props.user.cartDetail
         }))
-        .then(response => {
-            if(response.payload.onSuccess) {
-                setShowTotal(false)
-                setShowSuccess(true)
-            }
-        })
-        
-        
+            .then(response => {
+                if (response.payload.success) {
+                    setShowTotal(false)
+                    setShowSuccess(true)
+                }
+            })
     }
-    
+
+
+
     return (
         <div style={{ width: '85%', margin: '3rem auto' }}>
             <h1>My Cart</h1>
 
             <div>
-                <UserCardBlock products={props.user.cartDetail} removeItem={removeFromCart}/>
+                <UserCardBlock products={props.user.cartDetail} removeItem={removeFromCart} />
             </div>
-
 
             {ShowTotal ?
                 <div style={{ marginTop: '3rem' }}>
@@ -85,26 +84,23 @@ function CartPage(props) {
                 : ShowSuccess ?
                     <Result
                         status="success"
-                        title="Successfully Purchased Items!"
+                        title="Successfully Purchased Items"
                     />
                     :
                     <>
-                    <br />
-                    <Empty description={false} />
+                        <br />
+                        <Empty description={false} />
                     </>
             }
 
+
             {ShowTotal &&
-                <Paypal 
+                <Paypal
                     total={Total}
                     onSuccess={transactionSuccess}
                 />
-            
             }
-            
 
-
-            
         </div>
     )
 }
