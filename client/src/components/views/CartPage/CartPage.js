526e7b76270cd434a6f139105f5c80e7ce6d71d4
truth809 <truth809@naver.com>
2021-02-25 22:58:46 +0900

    13.finish

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -15,43 +15,42 @@ function CartPage(props) {
     useEffect(() => {
 
         let cartItems = []
-        //리덕스 User state안에 cart 안에 상품이 들어있는지 확인 
-        if (props.user.userData && props.user.userData.cart) {
-            if (props.user.userData.cart.length > 0) {
+
+        // 리덕스 user state 안에 cart 안에 상품이 들어있는지 확인
+        if(props.user.userData && props.user.userData.cart) {
+            if(props.user.userData.cart.length > 0) {
                 props.user.userData.cart.forEach(item => {
                     cartItems.push(item.id)
                 })
+
                 dispatch(getCartItems(cartItems, props.user.userData.cart))
-                    .then(response => { calculateTotal(response.payload) })
+                .then(response => { calculateTotal(response.payload)})
+                
             }
         }
     }, [props.user.userData])
-
 
     let calculateTotal = (cartDetail) => {
         let total = 0;
 
         cartDetail.map(item => {
-            total += parseInt(item.price, 10) * item.quantity
+            total += parseInt(item.price,10) * item.quantity
         })
-
         setTotal(total)
         setShowTotal(true)
-
     }
-
 
     let removeFromCart = (productId) => {
 
         dispatch(removeCartItem(productId))
-            .then(response => {
+        .then(response => {
 
-                if (response.payload.productInfo.length <= 0) {
-                    setShowTotal(false)
-                }
-
-            })
-
+            if(response.payload.productInfo.length <= 0) {
+                setShowTotal(false)
+            }
+            
+            
+        })
     }
 
     const transactionSuccess = (data) => {
@@ -59,16 +58,14 @@ function CartPage(props) {
             paymentData: data,
             cartDetail: props.user.cartDetail
         }))
-            .then(response => {
-                if (response.payload.success) {
-                    setShowTotal(false)
-                    setShowSuccess(true)
-                }
-            })
+        .then(response => {
+            if(response.payload.success) {
+                setShowTotal(false)
+                setShowSuccess(true)
+            }
+        })
     }
-
-
-
+    
     return (
         <div style={{ width: '85%', margin: '3rem auto' }}>
             <h1>My Cart</h1>
